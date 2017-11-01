f77ec175d3a8e0b739f095e4efe1cdfff033e97f
sonal18d <31699042+sonal18d@users.noreply.github.com>
2017-10-31 22:49:00 -0400

    Login UI changes
    Login UI changes

--- src/app/book-appointment/book-appointment.component.ts
+++ src/app/book-appointment/book-appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router'
 
 @Component({
   selector: 'app-book-appointment',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookAppointmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  backToService():void{
+    
+        this.router.navigate(['/services'])
+      }
 }
